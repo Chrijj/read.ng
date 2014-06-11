312dd615594674874a84c6c47ddf19668af6dee7
Chrijj <chrijj@gmail.com>
2014-06-11 22:05:39 +0100

    added deletion, adjusted formatting and flow

--- README.md
+++ README.md
@@ -6,8 +6,14 @@
  - start date
  - end date
 
-###stage two 
+###stage x
  - page count
  - description
  - review
  - time taken
+ - genre
+
+ 
+ #rev history
+ 0.2 - added book deletion, formatting and flow adjustments
+ 0.1 - basic list, file i/o, author title, start & finish date (string)
--- read_ng.py
+++ read_ng.py
@@ -23,6 +23,8 @@ class book(object):
 	def __str__(self):
 		return self.title + " - " + self.author + " (" + self.startDate + " to " + self.endDate + ")"
 
+print "=" * 45
+print "=" * 45
 print "Welcome to the book tracker"
 
 if os.path.isfile("my_books.pkl"):
@@ -38,14 +40,31 @@ else:
 user_input = ""
 
 while user_input != 'e':
-	print 'n to add, l to list, f to finish, e to exit'
+	print "-" * 55
+	print 'n to add, l to list, f to finish, d to delete, e to exit, c to cancel'
 	user_input = raw_input(":")
 	if user_input == "n":
-		title = raw_input("TITLE:")
-		author = raw_input("AUTHOR:")
-		startDate = raw_input("START DATE:")
-		newBook = book(title, author, startDate)
-		bookList.append(newBook)
+		user_input = raw_input("Add a book to the list?")
+		if user_input != "c":
+			title = raw_input("TITLE:")
+			author = raw_input("AUTHOR:")
+			startDate = raw_input("START DATE:")
+			newBook = book(title, author, startDate)
+			bookList.append(newBook)
+			title, "-", author, "started on", startDate, "added to bookList."
+	if user_input == 'd':
+		user_input = raw_input("TITLE TO DELETE:")
+		if user_input == "c":
+			pass
+		else:
+			if user_input in [x.title for x in bookList]:
+				bookPos = [x.title for x in bookList].index(user_input)
+				user_input = raw_input("AUTHOR FOR CONFIRMATION:")
+				if bookList[bookPos].author == user_input:
+					print bookList[bookPos].title, "-", bookList[bookPos].author, "deleted from bookList."
+					del bookList[bookPos]
+				else:
+					print "Deletion Cancelled."
 	if user_input == 'f':
 		title = raw_input("TITLE:")
 		for listed_book in bookList:
@@ -55,8 +74,8 @@ while user_input != 'e':
 		if bookList:
 			for listed_book in bookList:
 				print listed_book
-			else:
-				print "No books listed"
+		else:
+			print "No books listed"
 	if user_input == 'e':
 		pkl_out = open('my_books.pkl', 'wb')
 		pickle.dump(bookList, pkl_out)
